f301cc5fa9262fa57082bcfb6c6c11b6a8fd6494
Scott Munro <scottnmunro@gmail.com>
2016-03-29 17:54:04 -0500

    added  a script to link my config files

---
+++ install.sh
@@ -0,0 +1,7 @@
+ln -s $HOME/Development/environment-config/.vimrc $HOME/.vimrc
+
+ln -s $HOME/Development/environment-config/colormake.sh $HOME/colormake.sh
+
+ln -s $HOME/Development/environment-config/.zshrc $HOME/.zshrc
+
+ln -s $HOME/Development/environment-config/.tmux.conf $HOME/.tmux.conf
