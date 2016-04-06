7de814e9d98db82ea6e1f680321a188d3321437e
Scott Munro <scottnmunro@gmail.com>
2016-04-05 20:31:48 -0500

    adding a global git igore and updating the install script

--- install.sh
+++ install.sh
@@ -5,3 +5,5 @@ ln -s $HOME/Development/environment-config/colormake.sh $HOME/colormake.sh
 ln -s $HOME/Development/environment-config/.zshrc $HOME/.zshrc
 
 ln -s $HOME/Development/environment-config/.tmux.conf $HOME/.tmux.conf
+
+ln -s $HOME/Development/environment-config/.gitignore_global $HOME/.gitignore_global
