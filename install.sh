8adb98cc145958ba2960af3379ceebbe9d664cb8
Scott Munro <scottmunro@utexas.edu>
2016-04-07 10:52:14 -0500

    added install line

--- install.sh
+++ install.sh
@@ -6,4 +6,5 @@ ln -s $HOME/Development/environment-config/.zshrc $HOME/.zshrc
 
 ln -s $HOME/Development/environment-config/.tmux.conf $HOME/.tmux.conf
 
-ln -s $HOME/Development/environment-config/.gitignore_global $HOME/.gitignore_global
+ln -s $HOME/Development/environment-config/.gitignore_global $HOME/.gitignore_global && git config --global core.excludesfile ~/.gitignore_global
+
