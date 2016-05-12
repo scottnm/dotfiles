e6a081679e61958cbb6b57f772388894177af798
Scott Munro <scottmunro@utexas.edu>
2016-05-12 00:39:47 -0500

    added rainbow parens

--- install.sh
+++ install.sh
@@ -8,3 +8,6 @@ ln -s $HOME/Development/dotfiles/.tmux.conf $HOME/.tmux.conf
 
 ln -s $HOME/Development/dotfiles/.gitignore_global $HOME/.gitignore_global && git config --global core.excludesfile ~/.gitignore_global
 
+ln -s $HOME/Development/dotfiles/rainbow_parentheses.vim/autoload/rainbow_parentheses.vim $HOME/.vim/autoload/rainbow_parentheses.vim
+
+ln -s $HOME/Development/dotfiles/rainbow_parentheses.vim/plugin/rainbow_parentheses.vim $HOME/.vim/plugin/rainbow_parentheses.vim
--- uninstall.sh
+++ uninstall.sh
@@ -8,3 +8,6 @@ rm $HOME/.tmux.conf
 
 rm $HOME/.gitignore_global
 
+rm $HOME/.vim/autoload/rainbow_parentheses.vim
+
+rm $HOME/.vim/plugin/rainbow_parentheses.vim
