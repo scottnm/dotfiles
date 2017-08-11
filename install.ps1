257106bf7088f66e2e631b64a86fb1efe9d10401
Scott Munro <scmunro@microsoft.com>
2017-08-11 10:57:16 -0700

    updating install script for symlinks

--- install.ps1
+++ install.ps1
@@ -1,5 +1,4 @@
 mkdir ~\Documents\WindowsPowerShell
-cp config\Microsoft.PowerShell_profile.ps1 ~\Documents\WindowsPowerShell\
-.\config\vimplug_install.ps1
-cp config\_vimrc ~\_vimrc
+cmd /c mklink ~\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1 ~\Dev\dotfiles\config\Microsoft.PowerShell_profile.ps1
+cmd /c mklink ~\_vimrc ~\Dev\dotfiles\config\_vimrc
 git config --global core.editor "vim"
