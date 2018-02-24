89c64a60bc0e66f088bc1d60eb0735090a6d1a89
Scott Munro <scottnmunro@gmail.com>
2018-02-24 12:22:50 -0800

    updated vimrc junk

--- install.ps1
+++ install.ps1
@@ -5,15 +5,10 @@ $env:DotFilesPath = $env:HOME + "\Dev\dotfiles";
 $env:PSProfilePath = $env:DotFilesPath + "\config\ps_main.ps1";
 $env:WindowsPSProfileDir = $env:HOME + "\Documents\WindowsPowerShell";
 $env:WindowsPSProfilePath = $env:WindowsPSProfileDir + "\Microsoft.PowerShell_profile.ps1";
-
-echo "Ensuring PS folder exists...";
-mkdir $env:WindowsPSProfileDir;
-echo "Creating symlink for PS profile for $env:PSProfilePath $env:WindowsPSProfilePath";
 cmd /c mklink $env:WindowsPSProfilePath $env:PSProfilePath
-cmd /c echo "$env:WindowsPSProfilePath $env:PSProfilePath"
 
-#$env:WindowsVimConfPath = $env:HOME + "\_vimrc";
-#$env:VimConfPath = $env:DotFilesPath + "\config\_vimrc";
-#cmd /c mklink $env:WindowsVimConfPath $env:VimConfPath
+$env:WindowsVimConfPath = $env:HOME + "\_vimrc";
+$env:VimConfPath = $env:DotFilesPath + "\config\_vimrc";
+cmd /c mklink $env:WindowsVimConfPath $env:VimConfPath
 
-#git config --global core.editor "vim"
+git config --global core.editor "vim"
