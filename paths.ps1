d14881ff267a49ddb91a03e92b852440ecd38ca8
Scott Munro <scottnmunro@gmail.com>
2021-05-29 17:38:04 -0700

    add side path

--- paths.ps1
+++ paths.ps1
@@ -2,10 +2,12 @@ $env:DotFilesPath = "$home\Dev\dotfiles";
 
 # powershell profile paths
 $env:PSProfilePath = "$env:DotFilesPath\config\ps_main.ps1";
+$env:PSSideProfilePath = "$env:OneDrive\projects\dev\private_dotfiles\ps_side.ps1";
 $env:WindowsPSPath = "$home\Documents\WindowsPowerShell\";
 $env:WindowsPSCorePath = "$home\Documents\PowerShell\";
 $env:WindowsPSProfilePath = "$home\Documents\WindowsPowerShell\Microsoft.PowerShell_profile.ps1";
 $env:WindowsPSCoreProfilePath = "$home\Documents\PowerShell\Microsoft.PowerShell_profile.ps1";
+$env:WindowsPSSideProfilePath = "$env:DotFilesPath\config\ps_side.ps1"
 
 # Vim paths
 $env:WindowsVimConfPath = "$home\_vimrc";
